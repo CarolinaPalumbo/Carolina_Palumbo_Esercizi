020f4873c3e6169f3a3897c241d31e9d2cf8c4ca
Carolina <carolinapalumbo22@gmail.com>
2025-08-24 17:25:24 +0200

    dom2

---
+++ DOM1/dom1.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector("ul")
+console.log(ul.parentElement)
+
+console.log(ul.children[1])
+
+console.log(ul.children[1].nextElementSibling)
+
+console.log(ul.children[1].previousElementSibling)
---
+++ DOM2/dom2.js
@@ -0,0 +1,6 @@
+const paragrafi = document.querySelectorAll(".paragrafo");
+        console.log("Tutti i paragrafi:", paragrafi);
+        const elemento3 = document.getElementById("elemento-3");
+        console.log("Elemento con id elemento-3:", elemento3);
+        const titoliH2 = document.querySelectorAll("h2");
+        console.log("Tutti gli h2:", titoliH2);
